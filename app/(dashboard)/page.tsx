21061a210ea7feede8c67c90e546d4134be7037e
Muen Yu <parasomnia97@gmail.com>
2025-12-12 01:44:11 +1100

    image loading: eager

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
               </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-              <Image src="/mascot.webp" alt="WordCraft Mascot" width={700} height={600} />
+              <Image src="/mascot.webp" alt="WordCraft Mascot" loading='eager' width={700} height={600} />
             </div>
           </div>
         </div>
